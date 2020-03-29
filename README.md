d0a11eebd982f40538c91a7167e6b76d430e4e54
vrdvaibhav <vrdvaibhav@gmail.com>
2020-03-30 04:23:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ag-grid-angular-hello-world-vaibhav
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ag-grid-angular-hello-world-vaibhav)
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent  {
+
+    columnDefs = [
+        {field: 'make' },
+        {field: 'model' },
+        {field: 'price'}
+    ];
+
+    rowData = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ];
+
+}
